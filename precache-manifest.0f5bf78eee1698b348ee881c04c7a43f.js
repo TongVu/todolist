1de3ea437ad5674a939a0b18311c1154d3d90542
Tong Vu <tongvu.244@gmail.com>
2019-08-11 16:18:23 +0700

    Updates

---
+++ precache-manifest.0f5bf78eee1698b348ee881c04c7a43f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d760564ac72a425bbcd3f89a0d3cae",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "edc4723e42f1b4ff9747",
+    "url": "/todolist/static/css/main.647a1321.chunk.css"
+  },
+  {
+    "revision": "55a5847e7359dcc255a6",
+    "url": "/todolist/static/js/2.b90809b9.chunk.js"
+  },
+  {
+    "revision": "edc4723e42f1b4ff9747",
+    "url": "/todolist/static/js/main.8e8737d5.chunk.js"
+  },
+  {
+    "revision": "c9754b1a94c9501b6285",
+    "url": "/todolist/static/js/runtime~main.134556a3.js"
+  }
+]);
